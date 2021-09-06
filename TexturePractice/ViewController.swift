569c56d0e7c87b5199d500d3192702409a0c310c
darshilagrawal <darshilagrawal2000@gmail.com>
2021-09-06 23:47:05 +0530

    Practicing ASTableNode

--- TexturePractice/ViewController.swift
+++ TexturePractice/ViewController.swift
@@ -8,7 +8,44 @@
 import UIKit
 import AsyncDisplayKit
 
-class ViewController: ASViewController<ASCollectionNode>,ASCollectionDataSource,ASCollectionDelegate {
+class ViewController: ASDKViewController<ASTableNode> {
 
+    var tableNode:ASTableNode!
+    
+    override init() {
+        let tableNode = ASTableNode(style: .plain)
+        super.init(node: tableNode)
+
+        tableNode.dataSource = self
+        tableNode.delegate = self
+
+        self.tableNode = tableNode
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Storyboard ka Zamana Gaya Dost :)")
+    }
 }
 
+extension ViewController:ASTableDataSource,ASTableDelegate{
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableNode(_ tableNode: ASTableNode, nodeForRowAt indexPath: IndexPath) -> ASCellNode {
+        let cell = ASCellNode.init()
+        cell.backgroundColor = .black
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, didSelectRowAt indexPath: IndexPath) {
+        do {
+            tableNode.deselectRow(at: indexPath, animated: true)
+        }
+    }
+    
+}
