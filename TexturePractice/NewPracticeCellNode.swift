bd475ef0948bbd0d5ed8410276113fc776c4ff61
darshilagrawal <darshilagrawal2000@gmail.com>
2021-09-12 22:41:45 +0530

    Practice

--- TexturePractice/NewPracticeCellNode.swift
+++ TexturePractice/NewPracticeCellNode.swift
@@ -11,17 +11,23 @@ import AsyncDisplayKit
 class NewPracticeCellNode: ASCellNode {
    let textNode = ASTextNode()
     let imageNode = ASImageNode()
-    
+    let emptyNode = ASTextNode()
     init(text:String) {
         super.init()
-        self.textNode.attributedText = NSAttributedString(string: text)
+        let myAttribute = [ NSAttributedString.Key.font: UIFont(name: "Baskerville", size: 18.0)! ]
+        let attr = NSAttributedString(string: text,attributes: myAttribute)
+        self.textNode.attributedText = attr
+        let secondAttr = NSAttributedString(string: "Namaste",attributes: myAttribute)
+        self.emptyNode.attributedText = secondAttr
         self.imageNode.image = UIImage(named: "placeholderImage")
         backgroundColor = .cyan
         automaticallyManagesSubnodes = true
     }
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
         imageNode.contentMode = .scaleAspectFit
-        let firstPortion = ASStackLayoutSpec(direction: .vertical, spacing: 10, justifyContent: .center, alignItems: .center, children: [imageNode,textNode])
+        
+        let textStack = ASStackLayoutSpec(direction: .horizontal, spacing: 20, justifyContent: .start, alignItems: .center, children: [emptyNode,textNode])
+        let firstPortion = ASStackLayoutSpec(direction: .vertical, spacing: 0, justifyContent: .center, alignItems: .center, children: [imageNode,textStack])
         return firstPortion
     }
 }
